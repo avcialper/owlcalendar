851a67a7b9869be100aee3a654f0b26cfab16cd6
avcialper <avcialper12@gmail.com>
2025-01-09 11:33:31 +0300

    Added calendar header

--- owlcalendar/src/main/java/com/avcialper/owlcalendar/OwlCalendarView.kt
+++ owlcalendar/src/main/java/com/avcialper/owlcalendar/OwlCalendarView.kt
@@ -10,8 +10,10 @@ import com.avcialper.owlcalendar.adapter.calendar.CalendarAdapter
 import com.avcialper.owlcalendar.adapter.calendardayname.CalendarDayNameAdapter
 import com.avcialper.owlcalendar.databinding.CalendarBinding
 import com.avcialper.owlcalendar.manager.CalendarLayoutManager
+import com.avcialper.owlcalendar.util.constants.Constants
 import com.avcialper.owlcalendar.util.extensions.adjustStart
 import com.avcialper.owlcalendar.util.extensions.pushToastMessage
+import java.util.Locale
 
 class OwlCalendarView @JvmOverloads constructor(
     context: Context,
@@ -22,37 +24,72 @@ class OwlCalendarView @JvmOverloads constructor(
     private val binding: CalendarBinding
     private val jDateTime = JDateTime.instance
 
+    private var inputYear = jDateTime.year
+    private var inputMonth = jDateTime.month
+
     init {
         val layoutInflater = LayoutInflater.from(context)
         binding = CalendarBinding.inflate(layoutInflater, this, true)
 
+        setHeader()
+        initCalendarDayNameAdapter()
         initCalendarAdapter()
     }
 
-    private fun initCalendarAdapter() {
-        val dayNames = listOf("Pt", "Sa", "Ça", "Pe", "Cu", "Ct", "Pa")
-        val days = getDate()
-
-        val calendarDayNameAdapter = CalendarLayoutManager(context, VERTICAL)
+    private fun initCalendarDayNameAdapter() {
+        val dayNames = Constants.dayNames
         val calendarLayoutManager = CalendarLayoutManager(context, VERTICAL)
 
-        binding.apply {
-            rvCalendarDayName.apply {
-                layoutManager = calendarDayNameAdapter
-                adapter = CalendarDayNameAdapter(dayNames)
-            }
-            rvCalendar.apply {
-                layoutManager = calendarLayoutManager
-                adapter = CalendarAdapter(days, ::onDayClickListener)
-            }
+        binding.rvCalendarDayName.apply {
+            adapter = CalendarDayNameAdapter(dayNames)
+            layoutManager = calendarLayoutManager
+        }
+    }
+
+    private fun initCalendarAdapter() {
+        val days = getDate()
+        val calendarLayoutManager = CalendarLayoutManager(context, VERTICAL)
+
+        binding.rvCalendar.apply {
+            layoutManager = calendarLayoutManager
+            adapter = CalendarAdapter(days, ::onDayClickListener)
         }
     }
 
     private fun getDate(): List<JDayOfMonth?> {
-        val year = jDateTime.year
-        val month = jDateTime.month
+        val year = inputYear
+        val month = inputMonth
         val daysInMonth = jDateTime.getAllDaysOfMonth(year, month).adjustStart()
         return daysInMonth
+    }
+
+    private fun setHeader() {
+        val year = inputYear
+        val month = inputMonth
+
+        val monthName = Constants.monthNames[month]
+        val locale = Locale.getDefault()
+        binding.tvCalendarHeader.text = String.format(locale, "%s %d", monthName, year)
+    }
+
+    /**
+     * Sets the year of the calendar.
+     * @param year The year to set.
+     */
+    fun setYear(year: Int) {
+        inputYear = year
+        setHeader()
+        initCalendarAdapter()
+    }
+
+    /**
+     * Sets the month of the calendar.
+     * @param month The month to set. The month should be between 0 and 11.
+     */
+    fun setMonth(month: Int) {
+        inputMonth = month
+        setHeader()
+        initCalendarAdapter()
     }
 
     private fun onDayClickListener(day: JDayOfMonth) {
---
+++ owlcalendar/src/main/java/com/avcialper/owlcalendar/util/constants/Constants.kt
@@ -0,0 +1,20 @@
+package com.avcialper.owlcalendar.util.constants
+
+object Constants {
+    val dayNames = listOf("Pt", "Sa", "Ça", "Pe", "Cu", "Ct", "Pa")
+
+    val monthNames = listOf(
+        "Ocak",
+        "Şubat",
+        "Mart",
+        "Nisan",
+        "Mayıs",
+        "Haziran",
+        "Temmuz",
+        "Ağustos",
+        "Eylül",
+        "Ekim",
+        "Kasım",
+        "Aralık"
+    )
+}
